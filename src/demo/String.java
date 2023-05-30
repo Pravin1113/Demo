fe9669b60ab46aaa9a709bfd09172f20fa753ec3
Pravin1113 <pravin.chavan2729@gmail.com>
2023-05-30 13:57:08 +0530

    just changed

---
+++ src/demo/String.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+ java.lang.String d="abc";
+ java.lang.String b="abc";
+      boolean t=b.equals(d);
+      if(t==true) {
+    	  System.out.println("equals");
+      }
+      else {
+    	  System.out.println("not equal");
+      }
+	}
+
+}
